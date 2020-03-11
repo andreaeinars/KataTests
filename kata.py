d9a7e568efb957a0d074d709fcc50ea0d3fa2dde
Andrea Einarsdóttir <andreaeinars@hotmail.com>
2020-03-11 13:38:37 +0000

    Add supports an unknown number of arguments

--- kata.py
+++ kata.py
@@ -1,12 +1,23 @@
 
 def Add(numbers):
-    if len(numbers)==1:
-        return int(numbers)
 
-    if len(numbers)==3:
-        num1,num2=numbers.split(",")
-        return int(num1) + int(num2)
-
-    else:    
+    if numbers is "":
         return 0
+    
+    number_list=numbers.split(",")
+    number_list=[int(i) for i in number_list]
 
+    return sum(number_list)
+
+    #if len(numbers)==1:
+    #    return int(numbers)
+#
+    #if len(numbers)==3:
+    #    num1,num2=numbers.split(",")
+    #    return int(num1) + int(num2)
+#
+    #else:    
+    #    return 0
+
+
+print(Add("1,2,3,4"))
--- test_kata.py
+++ test_kata.py
@@ -8,5 +8,10 @@ def test_single_digit():
 
 def test_two_digits():
     assert Add("1,2")==3
+    assert Add("5,7")==12
+
+def test_unknown_num_of_digits():
+    assert Add("1,2,3,4,5")==15
+    assert Add("10,2,5,22,1,1")==41
 
 
